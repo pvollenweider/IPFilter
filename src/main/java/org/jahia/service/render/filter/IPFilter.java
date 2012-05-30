9887de84e6fca0a69daa61611fc711418b5c16df
Philippe Vollenweider <pvollenweider@jahia.com>
2012-05-30 10:31:03 +0200

    Change IP filtering to work on the full website. There is 2 type of filtering: Deny and Only allow

--- src/main/java/org/jahia/service/render/filter/IPFilter.java
+++ src/main/java/org/jahia/service/render/filter/IPFilter.java
@@ -4,7 +4,6 @@ package org.jahia.service.render.filter;
 import org.apache.commons.net.util.SubnetUtils;
 
 import org.jahia.exceptions.JahiaForbiddenAccessException;
-import org.jahia.services.content.JCRContentUtils;
 import org.jahia.services.content.JCRNodeWrapper;
 import org.jahia.services.render.RenderContext;
 import org.jahia.services.render.Resource;
@@ -12,8 +11,6 @@ import org.jahia.services.render.filter.AbstractFilter;
 import org.jahia.services.render.filter.RenderChain;
 import org.slf4j.Logger;
 
-import javax.jcr.RepositoryException;
-import java.util.Map;
 
 /**
  * Created by IntelliJ IDEA.
@@ -27,53 +24,48 @@ public class IPFilter extends AbstractFilter {
 
 
     public String prepare(RenderContext renderContext, Resource resource, RenderChain chain) throws Exception {
-        String address = renderContext.getRequest().getRemoteAddr();
-
-        JCRNodeWrapper currentNode = renderContext.getMainResource().getNode();
-
-        if ("127.0.0.1".equals(address)) {
-            // on localhost we use the testIP value as fake address
-            address = currentNode.hasProperty("testIp") ? currentNode.getProperty("testIp").getString().trim() : null;
-        }
-        if (address != null) {
-            boolean hasIPRestriction = checkPathRestriction(currentNode, address);
-            logger.debug("hasIPRestriction is " + hasIPRestriction);
-            if (hasIPRestriction) {
-                throw new JahiaForbiddenAccessException();
+        String currentAddress = renderContext.getRequest().getRemoteAddr();
+        JCRNodeWrapper siteNode = (JCRNodeWrapper) renderContext.getSite();
+        String filterType = siteNode.hasProperty("filterType") ? siteNode.getProperty("filterType").getString() : null;
+        if (filterType != null) {
+            if ("127.0.0.1".equals(currentAddress)) {
+                // on localhost we use the testIP value as fake currentAddress
+                currentAddress = siteNode.hasProperty("testIp") ? siteNode.getProperty("testIp").getString().trim() : null;
             }
-        }
-
-        return null;
-    }
-
-    private boolean checkPathRestriction(JCRNodeWrapper node, String address) {
-        String path = node.getPath();
-        try {
-            if (node.isNodeType("jnt:page")) {
-                String ipRangeList = node.hasProperty("ipRangeList") ? node.getProperty("ipRangeList").getString() : null;
+            if (currentAddress != null) {
+                String ipRangeList = siteNode.hasProperty("ipRangeList") ? siteNode.getProperty("ipRangeList").getString() : null;
                 if (ipRangeList != null) {
-                    String delimiter = ",";
-                    String[] ranges = ipRangeList.split(delimiter);
-                    for (int i = 0; i < ranges.length; i++) {
-                        String range = ranges[i].trim();
-                        SubnetUtils utils = new SubnetUtils(range);
-                        if (utils.getInfo().isInRange(address)) {
-                            logger.debug("Found IP restriction for page [" + path + "] : [" + address + "] is in subnet [" + range + "]");
-                            return true;
+                    boolean inRange = isInRange(currentAddress, ipRangeList);
+                    if ("deny".equals(filterType)) {
+                        if (inRange) {
+                            logger.warn("IPFilter - Deny rule: IP [" + currentAddress + "] is in subnet [" + ipRangeList + "]");
+                            throw new JahiaForbiddenAccessException();
                         }
-                    }
-                } else {
-                    logger.debug("No IP restrictions for page [" + path + "]. Checking parent");
-                    JCRNodeWrapper parentPage = JCRContentUtils.getParentOfType(node, "jnt:page");
-                    if (parentPage != null) {
-                        return checkPathRestriction(parentPage, address);
+                    } else { // onlyallow
+                        if (!inRange) {
+                            logger.warn("IPFilter - Only Allow rule:  IP [" + currentAddress + "] not in subnet [" + ipRangeList + "]");
+                            throw new JahiaForbiddenAccessException();
+                        }
                     }
                 }
             } else {
-                logger.debug("Node [" + path + "] is not a page");
+                logger.debug("IPFilter - Bypass IP filtering for localhost");
             }
-        } catch (RepositoryException e) {
-            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+        return null;
+    }
+
+    private boolean isInRange(String address, String ipRangeList) {
+        if (ipRangeList != null) {
+            String delimiter = ",";
+            String[] ranges = ipRangeList.split(delimiter);
+            for (int i = 0; i < ranges.length; i++) {
+                String range = ranges[i].trim();
+                SubnetUtils utils = new SubnetUtils(range);
+                if (utils.getInfo().isInRange(address)) {
+                    return true;
+                }
+            }
         }
         return false;
     }
